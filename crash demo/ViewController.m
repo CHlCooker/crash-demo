6d4dc354f94fe1069dcadf6667fd5752cf413b3d
CHlCooker <changhongliang@changhongliangdeMacBook-Pro.local>
2017-04-16 10:50:03 +0800

    第二次修改代码

--- crash demo/ViewController.m
+++ crash demo/ViewController.m
@@ -7,16 +7,7 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
-    NSString *str = @"https://www.baidu.com";
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:str]];
-    //    webView.delegate = self;
-    [self.view addSubview:webView];
-    [webView loadRequest:request];
-    
-}
+
 
 
 @end
